ff715bd68cba80b0905b191366dca759217b9f1c
Joey2001 <19arturajoseph@bprep.org>
2018-01-13 14:05:31 -0800

    Collision addition
    Added collision to main branch.

--- RainMakerXVIV/src/org/usfirst/frc/team360/robot/RobotMap.java
+++ RainMakerXVIV/src/org/usfirst/frc/team360/robot/RobotMap.java
@@ -8,10 +8,13 @@
 package org.usfirst.frc.team360.robot;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.SPI;
 
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
@@ -35,9 +38,60 @@ public class RobotMap {
 	public static Compressor compressor = new Compressor();
 	
 	public static PowerDistributionPanel pdp = new PowerDistributionPanel();
+	
+	public static AHRS navX = new AHRS(SPI.Port.kMXP);
 
 	public static TalonSRX motorL1 = new TalonSRX(0);
 	public static TalonSRX motorL2 = new TalonSRX(1);
 	public static TalonSRX motorR1 = new TalonSRX(2);
 	public static TalonSRX motorR2 = new TalonSRX(3);
+
+	public static float accelX;
+	public static float accelY;
+	
+	public static double[] someDistance;
+	public static Timer time;
+	
+	public static float Yaw;
+	public static float Pitch;
+	public static float Roll;
+	
+	public static double TotalYaw;
+	public static double YawRate;
+	
+//	public static float Compass;
+//	public static float Fused;
+	
+	public static boolean Moving;
+	public static boolean Rotating;
+	public static boolean Connected;
+	public static boolean Calibrating;
+	
+//	public static float VelocityX;
+//	public static float VelocityY;
+	
+//	public static float DisplacementX;
+//	public static float DisplacementY;
+	
+//	public static float RawGyroX;
+//	public static float RawGyroY;
+//	public static float RawGyroZ;
+	
+//	public static float RawAccelX;
+//	public static float RawAccelY;
+//	public static float RawAccelZ;
+	
+//	public static float RawMagX;
+//	public static float RawMagY;
+//	public static float RawMagZ;
+	
+//	public static float Temperature_Celsius;
+//	public static double Temperature_Fahrenheit;
+	
+	public static float QuaternionW;
+	public static float QuaternionX;
+	public static float QuaternionY;
+	public static float QuaternionZ;
+	
+	
 }
