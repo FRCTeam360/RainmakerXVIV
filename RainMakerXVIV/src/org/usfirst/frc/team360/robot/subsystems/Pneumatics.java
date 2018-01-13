38e488ebf7803ab5da4ccc033bdcee2a783f56d7
Gavin Kerr <30641267+GavinPlusPlus@users.noreply.github.com>
2018-01-12 16:43:18 -0800

    CAN drive
    I had nothing to do in this process besides uploading this. Way to go Joey

---
+++ RainMakerXVIV/src/org/usfirst/frc/team360/robot/subsystems/Pneumatics.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team360.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import org.usfirst.frc.team360.robot.*;
+import org.usfirst.frc.team360.commands.Pressurize;
+
+public class Pneumatics extends Subsystem {
+    Compressor comp = RobotMap.compressor;
+    public void pressurize (){
+    	comp.start();
+    }
+    public void stop(){
+    	comp.stop();
+    }
+    public void initDefaultCommand() {
+    	setDefaultCommand(new Pressurize());
+    }
+}
