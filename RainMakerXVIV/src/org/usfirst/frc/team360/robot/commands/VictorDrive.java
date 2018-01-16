d2ba5bd127ae53fbd325e6c2a445620e27a68103
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2018-01-15 16:24:21 -0800

    Github broke things
    I fixed it.

--- RainMakerXVIV/src/org/usfirst/frc/team360/robot/commands/VictorDrive.java
+++ RainMakerXVIV/src/org/usfirst/frc/team360/robot/commands/VictorDrive.java
@@ -25,23 +25,15 @@ public class VictorDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-<<<<<<< HEAD
-
+    	
     		if(joy.getRawButton(1)) {
     			VictorSPs.motor1.set(-0.2);
 
-=======
-    		if(joy.getRawButton(1)) {
-    			VictorSPs.motor1.set(-0.2);
->>>>>>> 6df9cd78316ad4778b3780155f83056a3d29f950
     		}
     		else {
     			VictorSPs.motor1.set(joy.getY());
     		}
-<<<<<<< HEAD
 
-=======
->>>>>>> 6df9cd78316ad4778b3780155f83056a3d29f950
     }
 
     // Make this return true when this Command no longer needs to run execute()
