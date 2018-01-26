cce97d5a243496411c2c9af47354565719915744
Frank4001 <35282266+Frank4001@users.noreply.github.com>
2018-01-26 14:32:36 -0800

    Ha Winch

---
+++ RainMakerXVIV/src/org/usfirst/frc/team360/robot/subsystems/Winch.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team360.robot.subsystems;
+
+import java.awt.Robot;
+import java.util.Set;
+
+import org.usfirst.frc.team360.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Winch extends Subsystem {
+	VictorSP Winch = RobotMap.motor1;
+	VictorSP Winch1 = RobotMap.motor2;
+	
+	
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+
+	public void setMotorspeed(double speed) {
+		Winch.set(speed);{
+	    Winch1.set(speed);
+	    }
+	}
+}
+
