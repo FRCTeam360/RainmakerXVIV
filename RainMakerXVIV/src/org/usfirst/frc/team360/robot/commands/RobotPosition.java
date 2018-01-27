642d05996c75e76389ab0446e5ceab382682a427
Frank4001 <35282266+Frank4001@users.noreply.github.com>
2018-01-27 11:43:49 -0800

    NavX

---
+++ RainMakerXVIV/src/org/usfirst/frc/team360/robot/commands/RobotPosition.java
@@ -0,0 +1,58 @@
+package org.usfirst.frc.team360.robot.commands;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class RobotPosition extends Command {
+	
+	  AHRS ahrs;
+	  
+    public RobotPosition() {
+    	
+    }
+    	  
+    	      
+    	// Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    
+	// Called just before this Command runs the first time
+    protected void initialize() {
+    	
+    	ahrs = new AHRS(SPI.Port.kMXP); 
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+	    	  SmartDashboard.putNumber(   "RawGyro_X",            ahrs.getRawGyroX());
+	          SmartDashboard.putNumber(   "RawGyro_Y",            ahrs.getRawGyroY());
+	          SmartDashboard.putNumber(   "RawGyro_Z",            ahrs.getRawGyroZ());
+	          SmartDashboard.putNumber(   "RawAccel_X",           ahrs.getRawAccelX());
+	          SmartDashboard.putNumber(   "RawAccel_Y",           ahrs.getRawAccelY());
+	          SmartDashboard.putNumber(   "RawAccel_Z",           ahrs.getRawAccelZ());
+	          SmartDashboard.putNumber(   "RawMag_X",             ahrs.getRawMagX());
+	          SmartDashboard.putNumber(   "RawMag_Y",             ahrs.getRawMagY());
+	          SmartDashboard.putNumber(   "RawMag_Z",             ahrs.getRawMagZ());
+	          SmartDashboard.putNumber(   "IMU_Temp_C",           ahrs.getTempC());
+	      
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
