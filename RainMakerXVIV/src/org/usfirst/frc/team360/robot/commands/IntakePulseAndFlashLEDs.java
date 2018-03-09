6da1e181edfa7b261f039d18b03a5da57d1651a8
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2018-03-08 16:59:21 -0800

    Revert "Changed ho long LED pulses red at start of IntakePulseAndFlashLEDs"
    This reverts commit a9b52f943425cb911b10daef525585552854ab64.

--- RainMakerXVIV/src/org/usfirst/frc/team360/robot/commands/IntakePulseAndFlashLEDs.java
+++ RainMakerXVIV/src/org/usfirst/frc/team360/robot/commands/IntakePulseAndFlashLEDs.java
@@ -32,7 +32,7 @@ public class IntakePulseAndFlashLEDs extends CommandGroup {
 //	    	addSequential(new IntakePulse(.75, 18, .25, true));
 //	    	addSequential(new IntakeClose());
 //	    	addSequential(new LEDColor(RobotMap.Color.GREEN, 1/3, 2, true, false));
-		addParallel(new LEDColor(RobotMap.Color.RED, 1.5, 10, true, false));
+		addParallel(new LEDColor(RobotMap.Color.RED, 1.5, 100, true, false));
     	addSequential(new IntakeClose());
     	addSequential(new IntakePulse(.75, 18, .25, true));
     	//addSequential(new LEDColor(RobotMap.Color.GREEN, 1/3, 2, true, false));
