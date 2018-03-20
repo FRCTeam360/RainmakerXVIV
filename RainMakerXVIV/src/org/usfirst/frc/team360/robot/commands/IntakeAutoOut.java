09366ab28f7e1dd8561103e8085f4379b8a318fc
Matthew Vandeberg <33138461+firerex145@users.noreply.github.com>
2018-03-19 18:57:04 -0700

    Manually updated FollowTrajectory and all Intake commands

--- RainMakerXVIV/src/org/usfirst/frc/team360/robot/commands/IntakeAutoOut.java
+++ RainMakerXVIV/src/org/usfirst/frc/team360/robot/commands/IntakeAutoOut.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class IntakeAutoOut extends CommandGroup {
 	
     public IntakeAutoOut() {
+    	addSequential(new IntakeAutoControl(-.5, true, .5));
 		addSequential(new IntakeOpen());
-		addSequential(new IntakeAutoControl(-.5, true, 1));
     }
 }
--- RainMakerXVIV/src/org/usfirst/frc/team360/robot/commands/IntakeIn.java
+++ RainMakerXVIV/src/org/usfirst/frc/team360/robot/commands/IntakeIn.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team360.robot.commands;
 
 
 import org.usfirst.frc.team360.robot.RobotMap;
-import org.usfirst.frc.team360.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -13,9 +12,7 @@ public class IntakeIn extends CommandGroup {
 	
     public IntakeIn() {
     	addParallel(new LEDColor(RobotMap.Color.GREEN, 1/3, 2, true, false));
-    	addSequential(new IntakeControl(.5, 25, true));
-    	addSequential(new IntakeClose());
-    	addSequential(new LEDColor(RobotMap.Color.GREEN, 0, 2, false, false));
-    	Robot.logger.logIntakeIn();
+    	addSequential(new IntakeOpen());
+    	addSequential(new IntakeControl(.5, 25, false));
     }
 }
